a9f075a1ce8c5534a3ade49eab1cc3ece6fdb789
emrullahtastan <emrullah69@gmail.com>
2024-03-22 16:36:30 +0300

    style props

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -7,6 +7,7 @@ import {Heading} from "./components/Heading";
 import {Oscar} from "./components/Oscar";
 import {Button} from "./components/Button";
 import {Input} from "./components/Input";
+import {Container} from "./components/Container";
 
 function App() {
     const personName = {first: 'Bruce', last: 'Wayne'}
@@ -30,6 +31,7 @@ function App() {
             <Input value={''} handleChange={(event) => {
                 console.log(event)
             }}/>
+            <Container styles={{border:'1px solid black', padding:'1rem'}}/>
         </div>
     );
 }
