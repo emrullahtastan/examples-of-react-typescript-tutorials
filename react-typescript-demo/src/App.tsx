82054dbbd86f3dd8d12964731417c27fe6f2a319
emrullahtastan <emrullah69@gmail.com>
2024-03-28 14:42:27 +0300

    useContext Hook

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,37 +1,12 @@
 import './App.css';
-import {Greet} from "./components/Greet";
-import {Person} from "./components/Person";
-import {PersonList} from "./components/PersonList";
-import {Status} from "./components/Status";
-import {Heading} from "./components/Heading";
-import {Oscar} from "./components/Oscar";
-import {Button} from "./components/Button";
-import {Input} from "./components/Input";
-import {Container} from "./components/Container";
-
+import {ThemeContextProvider} from "./components/context/ThemeContext";
+import {Box} from "./components/context/Box";
 function App() {
-    const personName = {first: 'Bruce', last: 'Wayne'}
-    const nameList = [{first: 'Bruce', last: 'Wayne'}, {first: 'Clark', last: 'Kent'}, {
-        first: 'Princess',
-        last: 'Diana'
-    }]
     return (
         <div className="App">
-            <Status status={'loading'}/>
-            <Heading>Placeholder</Heading>
-            <Oscar>
-                <Heading>Oscar goes to Leonardo Dispario!</Heading>
-            </Oscar>
-            <Greet name='Wishwas' isLoggedIn={false}/>
-            <Person name={personName}/>
-            <PersonList names={nameList}/>
-            <Button handleClick={(event, id) => {
-                console.log("Button clicked.", event, id)
-            }}/>
-            <Input value={''} handleChange={(event) => {
-                console.log(event)
-            }}/>
-            <Container styles={{border:'1px solid black', padding:'1rem'}}/>
+            <ThemeContextProvider>
+                <Box/>
+            </ThemeContextProvider>
         </div>
     );
 }
