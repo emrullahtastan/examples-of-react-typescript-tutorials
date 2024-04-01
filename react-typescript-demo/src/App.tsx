4a4113998ca058186502dd9fee91604aac9fb47d
emrullahtastan <emrullah69@gmail.com>
2024-04-01 16:23:38 +0300

    Class Component

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,12 +1,9 @@
 import './App.css';
-import {ThemeContextProvider} from "./components/context/ThemeContext";
-import {Box} from "./components/context/Box";
+import {Counter} from "./components/class/Counter";
 function App() {
     return (
         <div className="App">
-            <ThemeContextProvider>
-                <Box/>
-            </ThemeContextProvider>
+           <Counter message={'This is a message'}/>
         </div>
     );
 }
