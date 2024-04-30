8ffb8ce3c56da02e4da282515b726367fc2a4a57
emrullahtastan <emrullah69@gmail.com>
2024-04-30 15:29:11 +0300

    Generic Props

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
-import {Profile} from "./components/auth/Profile";
-import {Private} from "./components/auth/Private";
+import {List} from './components/generics/List'
 
 function App() {
     return (
         <div className="App">
-            <Private isLoggedIn={true} component={Profile}/>
+            {/*<List items={['Batman', 'Superman', 'Wonder Woman']} onClick={(item) => console.log(item)}/>
+            <List items={[1, 2, 3]} onClick={(item) => console.log(item)}/>*/}
+            <List items={[{id:1, first: 'Bruce', last: 'Wayne'}, {id:2, first: 'Clark', last: 'Kent'}, {
+                id:3,
+                first: 'Princess',
+                last: 'Diana'
+            }]} onClick={(item) => console.log(item)}/>
         </div>
     );
 }
